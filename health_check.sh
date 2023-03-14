ed51c7392eb0aba1e78193a6a16f27ded80aa6a2
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-03-15 01:58:43 +0800

    script health check

--- health_check.sh
+++ health_check.sh
@@ -5,7 +5,9 @@ response=$(curl --silent http://localhost:8080/health)
 
 # Check if the response string equals 'ok'
 if [[ "$response" == "ok" ]]; then
+    echo "Health check passed"
     exit 0
 else
+    echo "Health check failed"
     exit 1
 fi
